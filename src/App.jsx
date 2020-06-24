b8793d6dbfb4b9671856b5f0838f34baa2b92e2b
CodyBanks <codydouglasbanks@gmail.com>
2020-06-24 14:29:11 -0400

    Extracted a Movie Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,35 @@
 import React, { Component } from 'react'
 
+class Movie extends Component {
+  render() {
+    return (
+      <li className="media">
+        <img src="..." class="mr-3" alt="..." />
+        <div className="media-body">
+          <h5 className="mt-0 mb-1">List-based media object</h5>
+          Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+          scelerisque ante sollicitudin. Cras purus odio, vestibulum in
+          vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi
+          vulputate fringilla. Donec lacinia congue felis in faucibus.
+        </div>
+      </li>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <main className="container p-4">
-        <div class="jumbotron">
-          <h1 class="display-4">Party Like Its 1989</h1>
-          <p class="lead">Showcase of the most popular movies of 1989.</p>
+        <div className="jumbotron">
+          <h1 className="display-4">Party Like Its 1989</h1>
+          <p className="lead">Showcase of the most popular movies of 1989.</p>
         </div>
-        <ul class="list-unstyled">
-          <li class="media">
-            <img src="..." class="mr-3" alt="..." />
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">List-based media object</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-              vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-          </li>
-          <li class="media my-4">
-            <img src="..." class="mr-3" alt="..." />
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">List-based media object</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-              vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-          </li>
-          <li class="media">
-            <img src="..." class="mr-3" alt="..." />
-            <div class="media-body">
-              <h5 class="mt-0 mb-1">List-based media object</h5>
-              Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
-              scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-              vulputate at, tempus viverra turpis. Fusce condimentum nunc ac
-              nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-          </li>
+        <ul className="list-unstyled">
+          <Movie />
+          <Movie />
+          <Movie />
+          <Movie />
         </ul>
       </main>
     )
