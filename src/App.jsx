ba5d78d8118cd7643cb66d0d59892ffe381a27a2
CodyBanks <codydouglasbanks@gmail.com>
2020-06-24 18:44:02 -0400

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ class App extends Component {
 
     return (
       <main className="container p-4">
-        <div className="jumbotron bg-danger">
+        <div className="jumbotron">
           <h1 className="display-4 ">Party Like Its 1989</h1>
           <p className="lead ">Showcase of the most popular movies of 1989</p>
         </div>
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -3,7 +3,7 @@ export class Movie extends Component {
   render() {
     const { title, poster_path, overview } = this.props
     return (
-      <li className="media bg-warning p-4">
+      <li className="media p-4">
         <img
           src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2${poster_path}`}
           width="200"
